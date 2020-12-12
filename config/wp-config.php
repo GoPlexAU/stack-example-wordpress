dc66450083e3c8d47328bde63288fb772f10a469
bharrold <blake@goplex.com.au>
2020-12-12 20:45:56 +1100

    Update wp-config.php

--- config/wp-config.php
+++ config/wp-config.php
@@ -3,7 +3,7 @@
 	define( 'DISALLOW_FILE_EDIT', false );
 	define( 'DISALLOW_FILE_MODS', false );
 
-	define( 'WP_DEBUG', true );
+	define( 'WP_DEBUG', false );
 	define( 'SAVEQUERIES', true );
 /*
 if ( 'development' === getenv('WP_ENV') ) {
