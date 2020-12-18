bfa2d0fc8851f08ff1b42b10f61eaa454ab6e383
bharrold <blake@goplex.com.au>
2020-12-18 18:49:40 +1100

    wp-config updates

--- config/wp-config.php
+++ config/wp-config.php
@@ -1,10 +1,13 @@
 <?php
 // Set custom wp-config.php settings in this file 
-	define( 'DISALLOW_FILE_EDIT', false );
 	define( 'DISALLOW_FILE_MODS', false );
 
-	define( 'WP_DEBUG', false );
-	define( 'SAVEQUERIES', true );
+	define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+@ini_set( 'display_errors', 1 );
+define( 'SCRIPT_DEBUG', true );
+
 /*
 if ( 'development' === getenv('WP_ENV') ) {
 	define( 'DISALLOW_FILE_EDIT', false );
